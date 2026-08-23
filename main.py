4054f05b5a0bfb1af6c71e0b028836edabfd6fca
Umut Özkan <umutozkan123456@gmail.com>
2026-08-23 11:17:49 +0300

    get_book_test() added

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Bookbot Project")
+filepath = "books/frankenstein.txt"
+
+def get_book_test(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        return file_contents
+
+
+print(get_book_test(filepath))
