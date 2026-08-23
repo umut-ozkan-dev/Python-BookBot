caf894106261e2b78e91d5df0500373791603ee6
Umut Özkan <umutozkan123456@gmail.com>
2026-08-23 11:31:47 +0300

    wordcount() function

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 filepath = "books/frankenstein.txt"
 
+
 def get_book_test(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
 
 
-print(get_book_test(filepath))
+def main():
+    file_contents = get_book_test(filepath)
+    print(file_contents)
+
+def wordcount(filepath):
+    text = get_book_test(filepath)
+    words = text.split()
+    print(f"Found {len(words)} total words")
+
+# main()
+
+wordcount(filepath)
